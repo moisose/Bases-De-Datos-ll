8ef70a4c791583debbc8fb5bcafe071766f6e9cf
JoxanF <joxan@estudiantec.cr>
2023-02-27 13:50:53 -0600

    Update orchestrator.py
     the orchestrator script is formated to be a function, has the same format of function declaration as script.py

--- Proyecto Opcional/Python/orchestrator.py
+++ Proyecto Opcional/Python/orchestrator.py
@@ -4,38 +4,38 @@
 
 import requests
 from bs4 import BeautifulSoup
-import time
 from datetime import date
 
-# root of the NOOA website to add to the files url
-root = 'https://www.ncei.noaa.gov/pub/data/ghcn/daily/all/'
 
-begin = time.time()
+# lists the file's links in gov/pub/data/ghcn/daily/all/
+# @restrictions: none
+# @param: none
+# @output: none
+def readFolder():
+    # root of the NOOA website to add to the files url
+    root = 'https://www.ncei.noaa.gov/pub/data/ghcn/daily/all/'
 
-# Making a GET request
-r = requests.get('https://www.ncei.noaa.gov/pub/data/ghcn/daily/all/')
- 
-# Parsing the HTML
-soup = BeautifulSoup(r.content, 'html.parser')
+    # Making a GET request
+    r = requests.get('https://www.ncei.noaa.gov/pub/data/ghcn/daily/all/')
+    
+    # Parsing the HTML
+    soup = BeautifulSoup(r.content, 'html.parser')
 
-end = time.time()
+    # find all the anchor tags with "href"
+    # the count is used to ommit the first 5 links
+    count = 0
+    for link in soup.find_all('a', href=True):
+        if count > 4:
+            name = link['href']
+            url = root + link['href']
+            processedDay = date.today()
+            state = 'LISTED'
+            md5 = None
 
-print('Execution Time: ' + str(end-begin))
+            # code to save the record and publish the ms in rabbitMQ here
 
-# find all the anchor tags with "href"
-cont = 0
-
-for link in soup.find_all('a', href=True):
-    if cont > 4:
-        name = link['href']
-        url = root + link['href']
-        processedDay = date.today()
-        state = 'LISTED'
-        md5 = None
-        
-        print(name, url, processedDay, state, md5)
-        print()
-    if cont == 40:
-        break
-    cont += 1
+        if count == 40:
+            break
+        count += 1
+    r.close()
 
