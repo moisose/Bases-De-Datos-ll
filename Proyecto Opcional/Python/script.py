750bd9d25915b4633d79d6a254c6a8e7be07a756
Fiorella Zelaya <fiorella.zelaya1024@estudiantec.cr>
2023-02-27 18:46:22 -0600

    Fix errors and insert files data into filetext table

--- Proyecto Opcional/Python/script.py
+++ Proyecto Opcional/Python/script.py
@@ -6,8 +6,8 @@ import hashlib
 
 
 
-pw = 'MhlDahiana'
-puerto = '3307'
+pw = 'MARIPOSA24'
+puerto = '3306'
 
 
 
@@ -16,26 +16,34 @@ puerto = '3307'
 # @param: the name of the stored prcedure and the parameters of the stored procedure (array of strings)
 # @output: none
 def executeProcedure(procedure, parameters):
-   
+    resultArray = []
     try:
         conn = mysql.connector.connect(host="localhost", user='root', password= pw, port= puerto, database='weather')
         cursor = conn.cursor()
         args = ("FF", 2, 2, 20, 3)
         result_args = cursor.callproc(procedure, parameters)
-        for result in cursor.stored_results():
-             print(result.fetchall())
-        conn.commit()
+
         
+        for result in cursor.stored_results():
+             resultArray.append(result.fetchall())
+             #print(resultArray)
+        conn.commit()
 
-    except mysql.connector.Error as error:
-        print("Failed to execute stored procedure: {}".format(error))
-
-    finally:
         if (conn.is_connected()):
             cursor.close()
             conn.close()
-            print("MySQL connection is closed")
-    return cursor
+            stored_results = cursor.stored_results()
+            #print("MySQL connection is closed")
+        
+
+    except mysql.connector.Error as error:
+        pass
+        #print("Failed to execute stored procedure: {}".format(error))
+
+    finally:
+        pass
+        
+    return resultArray
  
 
 # reads countries.txt and inserts in table weather.countries
@@ -48,10 +56,12 @@ def readCountries():
     
     string = file.content.decode('utf-8')
     lines = string.rsplit('\n')
+    
     md5 = getMd5(string)
-    cursor = executeProcedure('loadFile', ["ghcnd-countries.txt", url, str(md5), "Descargado"])
-    for result in cursor.stored_results():
-        print("Resultado", result.fetchall())
+    stored_results = executeProcedure('loadFile', ["ghcnd-countries.txt", url, str(md5).encode(), "Descargado"])
+    print(stored_results)
+    
+    for result in stored_results:
         if result[0][0] == "The file has been created" or result[0][0] == 'The textFile has been successfully modified.':
             for line in lines:
                 code = line[:2]
@@ -77,11 +87,18 @@ def readStates():
     string = file.content.decode('utf-8')
     lines = string.rsplit('\n')
 
-    for line in lines:
-        code = line[:2]
-        name = line[3:]
-        executeProcedure('createState', [code, name])
-
+    md5 = getMd5(string)
+    stored_results = executeProcedure('loadFile', ["ghcnd-states.txt", url, str(md5).encode(), "Descargado"])
+    print(stored_results)
+    
+    for result in stored_results:
+        if result[0][0] == "The file has been created" or result[0][0] == 'The textFile has been successfully modified.':
+            for line in lines:
+                code = line[:2]
+                name = line[3:]
+                executeProcedure('createState', [code, name])
+        else:
+            print("El archivo no se modifico")
 
     file.close()
     
@@ -97,21 +114,27 @@ def readStations():
     string = file.content.decode('utf-8')
     lines = string.rsplit('\n')
 
-    for line in lines:
-        print('\n' + line)
-        stationId = line[:11]
-        countryCode = stationId[0:2]
-        latitude = line[12:20].replace(' ', '')
-        longitude = line[21:30].replace(' ', '')
-        elevation = line[31:37].replace(' ', '')
-        state = line[38:40].replace(' ', '')
-        name = line[41:71]
-        gsnFlag = line[72:75].replace(' ', '')
-        hcnFlag = line[76:79].replace(' ', '')
-        wmoId = line[80:85].replace(' ', '')
-        
-        executeProcedure('createStation', [stationId, latitude, longitude, elevation, state, name, gsnFlag, hcnFlag, wmoId, countryCode])
-
+    md5 = getMd5(string)
+    stored_results = executeProcedure('loadFile', ["ghcnd-stations.txt", url, str(md5).encode(), "Descargado"])
+    print(stored_results)
+    
+    for result in stored_results:
+        if result[0][0] == "The file has been created" or result[0][0] == 'The textFile has been successfully modified.':
+            for line in lines:
+                stationId = line[:11]
+                countryCode = stationId[0:2]
+                latitude = line[12:20].replace(' ', '')
+                longitude = line[21:30].replace(' ', '')
+                elevation = line[31:37].replace(' ', '')
+                state = line[38:40].replace(' ', '')
+                name = line[41:71]
+                gsnFlag = line[72:75].replace(' ', '')
+                hcnFlag = line[76:79].replace(' ', '')
+                wmoId = line[80:85].replace(' ', '')
+                
+                executeProcedure('createStation', [stationId, latitude, longitude, elevation, state, name, gsnFlag, hcnFlag, wmoId, countryCode])
+        else:
+            print("El archivo no se modifico")
 
     file.close()
 
@@ -120,8 +143,8 @@ def readStations():
 def getMd5(string):
     hashSha = hashlib.sha256()
     hashSha.update(string.encode())
-    print (hashSha.hexdigest())
-    return hashSha
+    #print (hashSha.hexdigest())
+    return hashSha.hexdigest()
     
    
 
