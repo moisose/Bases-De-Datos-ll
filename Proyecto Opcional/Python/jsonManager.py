425ff1c4b6d38d5953b850eabd5a378875742b47
Isaac Araya Solano <isaacaraya@estudiantec.cr>
2023-03-01 15:07:45 -0600

    added transformation first test method

--- Proyecto Opcional/Python/jsonManager.py
+++ Proyecto Opcional/Python/jsonManager.py
@@ -1,5 +1,8 @@
 import json
 
+"""
+This method receives the name of a file and its contents and creates a file called processor.json
+"""
 def processorJson(name, contents):
     result = {"filename" : name, "contents": contents}
 
@@ -7,6 +10,11 @@ def processorJson(name, contents):
         json.dump(result, write_file)
 
     return result
+
+
+"""
+This method receives a filename and a list of data and creates a file called parser.json
+"""
 
 def parserJson(filename, listOfData):
     
@@ -30,3 +38,35 @@ def parserJson(filename, listOfData):
         json.dump(result, write_file)
 
     return result
+
+
+"""
+This method receives a json and transform its data and creates a file called transformation.json
+"""
+
+
+def transformationJson(jsonParsed):
+    for i in json["data"]:
+        date = i["date"]
+        stationId = i["station_id"]
+
+        tempDict = {
+            "station_id": i[0],
+            "date": i[1],
+            "month": date[0:4],
+            "year": date[4:6], 
+            "type": i[2],
+            "value": i[3],
+            "mflag": i[4],
+            "qflag": i[5],
+            "sflag": i[6],
+            "FIPS_country_code" : stationId[0:2],
+            "network_code" : stationId[2:3],
+            "real_station_id" : stationId[3:12]
+        }
+
+        i = tempDict
+
+    with open("transformation.json", "w") as write_file:
+        json.dump(jsonParsed, write_file)
+
