480248a51255cd3e917d7ddd800743c706f97f81
moisose <moises.solano@estudiantec.cr>
2023-03-02 09:04:44 -0600

    working in parser and a example of 2 cronjobs were added to WindyUi

--- Proyecto Opcional/WindyUI/docker/build.sh
+++ Proyecto Opcional/WindyUI/docker/build.sh
@@ -5,6 +5,11 @@ sudo docker build -t moisose/componente1 .
 sudo docker images
 sudo docker push moisose/componente1
 cd ..
+cd componente2
+sudo docker build -t moisose/componente2 .
+sudo docker images
+sudo docker push moisose/componente2
+cd ..
 # cd componente2
 # sudo docker build -t username/componente2 .
 # sudo docker images
