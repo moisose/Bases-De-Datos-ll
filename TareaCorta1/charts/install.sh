263d0a59922be775bbc485060b7db80d2e63b91f
Moises Solano <90010406+moisose@users.noreply.github.com>
2023-03-09 00:11:17 -0600

    install, uninstall and updateDependencies.sh files created. Also added the JSON files for the grafana dashboard

---
+++ TareaCorta1/charts/install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# cd app
+# helm dependency build --skip-refresh
+# cd ../bootstrap
+# helm dependency build --skip-refresh
+# cd ../databases
+# helm dependency build --skip-refresh
+# cd ../grafana-config
+# helm dependency build --skip-refresh
+# cd ../monitoring-stack
+# helm dependency build --skip-refresh
+# cd ..
+helm upgrade --install bootstrap bootstrap
+sleep 5
+helm upgrade --install monitoring-stack monitoring-stack
+sleep 5
+helm upgrade --install databases databases
+sleep 5
+helm upgrade --install grafana-config grafana-config
