80a0dc11c1d80e004e1b759429a7e7416487d83f
Isaac4918 <isaac4918@gmail.com>
2023-03-10 11:03:05 -0600

    modified documentation and added images to the documentation/resources forlder

--- Proyecto Opcional/Python/jsonManager.py
+++ Proyecto Opcional/Python/jsonManager.py
@@ -52,8 +52,6 @@ def parserJson(filename, listOfData):
 """
 This method receives a json and transform its data and creates a file called transformation.json
 """
-
-
 def transformationJson(jsonParsed):
     for i in json["data"]:
         date = i["date"]
