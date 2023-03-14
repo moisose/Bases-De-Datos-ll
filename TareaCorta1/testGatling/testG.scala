4b310bd9bbc0fe0a4ec19b53232f173da26143e4
MelSaFer <dhnmelfer@estudiantec.cr>
2023-03-13 23:54:52 -0600

    first test with gatling

---
+++ TareaCorta1/testGatling/testG.scala
@@ -0,0 +1,28 @@
+/*
+testG.scala
+
+gatling-charts-highcharts-bundle-3.9.2/users-files/simulation
+
+Import of libs
+*/
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import scala.concurrent.duration._
+
+/*
+Class testDataset
+*/
+class testDataset extends Simulation {
+  /*define http dir*/
+  val httpConf = http.baseUrl("http://127.0.0.1:5000")
+  /*define scenario*/
+  val scn = scenario("escenario")
+    .exec(http("peticion")
+      .get("/")
+      .check(status.is(200)) /*Verifies status equals 200*/
+    )
+  /*Users per second*/
+  setUp(
+    scn.inject(constantUsersPerSec(5) during (30 seconds))
+  ).protocols(httpConf)
+}
