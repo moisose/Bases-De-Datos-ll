732f0b11e2ddabc389aa4fb799f23dba1eac5181
Isaac Araya Solano <isaacaraya@estudiantec.cr>
2023-03-15 21:46:00 -0600

    Slight modifications to the MariaDB API and tests

--- TareaCorta1/pythonTests/test.py
+++ TareaCorta1/pythonTests/test.py
@@ -1,43 +1,80 @@
 import requests
 import threading
 import time
+import json
+
 
 BASE = "http://127.0.0.1:5000/"
 
-listaNombres = ["Isaac Araya", "Fiorella Zelaya", "Melany Salas", "Moisés Solano", "Pablo Arias"]
 
-def test1():
-    while(True):
-        response = requests.get(BASE)
-        print("==>")
-        print(response.json())
-        time.sleep(1)
+def testLoad():
+    url = BASE + "babynames"
+    data = {"birthyear": 1990, "gender": "F", "ethnicity": "Hispanic", "nm": "Carlos", "cnt": 2, "rnk": 3}
 
-def test2():
-    while(True):
-        response = requests.get(BASE)
-        print("-->")
-        print(response.json())
-        time.sleep(1)
-
-def test3():
-    #response = requests.post(BASE + "babynames/{'birthyear':1990, 'gender':'F', 'ethnicity':'Asian', 'nm':1, 'cnt':2, 'rnk':3}")
-    response = requests.get(BASE + "babynames/")
+    headers = {'Content-type': 'application/json'}
+    response = requests.post(url, data=json.dumps(data), headers=headers)
     print(response)
-    # for i in listaNombres:
+    print(response.json())
+
+def testUpdate(jsonInput):
+    url = BASE + "babynames"
+    data = jsonInput
+
+    headers = {'Content-type': 'application/json'}
+    response = requests.put(url, data=json.dumps(data), headers=headers)
+    print(response)
+    print(response.json())
+
+def testDelete(id):
+    url = BASE + "babynames"
+    data = {"id": id}
+
+    headers = {'Content-type': 'application/json'}
+    response = requests.delete(url, data=json.dumps(data), headers=headers)
+    print(response)
+    print(response.json())
+
+def testRead():
+    response = requests.get(BASE + "babynames")
+    print(response)
+    print(response.json())
+
+
+
+def menu():
+    while True:
+        print("Bienvenido, estos son los test disponibles:")
+        print("1.Read Test")
+        print("2.Load Test")
+        print("3.Update Test")
+        print("4.Delete Test")
+        opcion = int(input("Ingrese la opción que desea usar: "))
         
-    #     print("==>")
-    #     print(response.json())
-    #     time.sleep(1)
+        if opcion == 1:
+            testRead()
 
-thread1 = threading.Thread(target=test1)
-thread2 = threading.Thread(target=test2)
-thread3 = threading.Thread(target=test3)
+        elif opcion == 2:
+            testLoad()
 
-#thread1.start()
-#thread2.start()
-#thread3.start()
-test3()
+        elif opcion == 3:
+            id = int(input("Ingrese el id del que desea actualizar: "))
+            birthyear = int(input("Ingrese el id del que desea actualizar: "))
+            gender = input("Ingrese el id del que desea actualizar: ")
+            ethnicity = input("Ingrese el id del que desea actualizar: ")
+            nm = input("Ingrese el id del que desea actualizar: ")
+            cnt = int(input("Ingrese el id del que desea actualizar: "))
+            rnk = int(input("Ingrese el id del que desea actualizar: "))
+            jsonInput = {"id": id, "birthyear": birthyear, "gender": gender, "ethnicity": ethnicity, "nm": nm, "cnt": cnt, "rnk": rnk}
+            testUpdate(jsonInput)
+
+        elif opcion == 4:
+            id = int(input("Ingrese el id del que desea eliminar: "))
+            testDelete(id)
+
+        else:
+            print("La opción no es válida.")
+
+menu()
 
 
 """
@@ -51,4 +88,6 @@ docker run -p 8080:5000 imagen-docker
 En este ejemplo, el puerto 8080 de la máquina local se mapeará al puerto 5000 del contenedor. Ahora puedes acceder a la aplicación web desde tu navegador web ingresando la dirección http://localhost:8080.
 
 Es importante tener en cuenta que puedes mapear cualquier número de puertos del contenedor al puerto local utilizando la opción -p. Solo necesitas especificar el puerto local y el puerto del contenedor para cada mapeo de puerto que desees establecer.
+
+
 """
