48dd4c67b7e80f0b7bda57d617ff76d67e792fc5
Moises Solano <90010406+moisose@users.noreply.github.com>
2023-03-24 11:30:34 -0600

    mariadbgalera and postgresha deployments for the apis

--- TareaCorta1/charts/install.sh
+++ TareaCorta1/charts/install.sh
@@ -16,7 +16,7 @@ sleep 5
 helm upgrade --install monitoring-stack monitoring-stack
 sleep 5
 helm upgrade --install databases databases
-sleep 60
+sleep 30
 helm upgrade --install stateless stateless -f databases/values.yaml
 sleep 5
 helm upgrade --install grafana-config grafana-config
