9fd5ac0cd32f5fefc0b5a666245b7bee752ad8ce
Isaac Araya Solano <isaacaraya@estudiantec.cr>
2023-04-13 18:54:32 -0600

    connection to cassandra completed

--- Proyecto1/docker/BlobStorage/app/app.py
+++ Proyecto1/docker/BlobStorage/app/app.py
@@ -9,13 +9,31 @@ Se supone que es el más grande
 """
 
 from flask import Flask
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 
 app = Flask(__name__)
 api = Api(app)
 
+"""
+
+{'userId': 'user1', 'log': 'File loaded'}
+
+parser = {'userId': None}
+
+"""
+
+parser = reqparse.RequestParser()
+parser.add_argument('userId', type=str, help='User ID', required=True)
+parser.add_argument('name', type=str, help='User Name', required=False)
+parser.add_argument('age', type=int, help='Age', required=False)
+
+
 class BlobStorage(Resource):
     def get(self):
+        args = parser.parse_args()
+        userId = args['userId']
+        name = args['name']
+        conn.callproc("sp_insert_user", (userId,name))
         return {'message': 'Hello, World!'}
     def post(self):
         return {'message': 'Hello, World!'}
--- Proyecto1/docker/Cassandra/app/app.py
+++ Proyecto1/docker/Cassandra/app/app.py
@@ -7,40 +7,59 @@ Debe solicitar siempre el usuario y luego lo que se quiera agregar al log.
 """
 
 from cassandra.cluster import Cluster
+from cassandra.auth import PlainTextAuthProvider
+from ssl import SSLContext, PROTOCOL_TLSv1_2, CERT_NONE
 
 
 # Define el string de conexión a Cosmos DB de Cassandra
 contact_points = ['tfex-cosmos-db-28370.cassandra.cosmos.azure.com']
 port = 10350
-username = 'tfex-cosmos-db-28370'
-password = 'dGLl4TrKU5tA3cg4VKHuz5lMvePtHgmp9fxB5x2GTKpdl6LReTyztzLjR3EkVwNHnAn3M4enzs0GACDbUhI7LQ=='
+usernameVar = 'tfex-cosmos-db-28370'
+passwordVar = 'dGLl4TrKU5tA3cg4VKHuz5lMvePtHgmp9fxB5x2GTKpdl6LReTyztzLjR3EkVwNHnAn3M4enzs0GACDbUhI7LQ=='
 keyspace = 'tfex-cosmos-cassandra-keyspace'
+
+auth_provider = PlainTextAuthProvider(username=usernameVar, password=passwordVar)
+ssl_context = SSLContext(PROTOCOL_TLSv1_2)
+ssl_context.verify_mode = CERT_NONE
 
 # Crea un objeto Cluster y una sesión de Cassandra
 cluster = Cluster(
     contact_points=contact_points, 
     port=port, 
-    auth_provider=(
-        ('username', username), 
-        ('password', password)
-    ),
-    ssl_context=True
+    auth_provider=auth_provider,
+    ssl_context=ssl_context
 )
 session = cluster.connect(keyspace)
 
+# 'tfex-cosmos-cassandra-keyspace'.
 # Define una clase de recurso que utiliza la sesión de Cassandra para hacer consultas
 class CassandraConnector():
-    def subidaDeArchivo(self, user):
-        session.execute('SELECT * FROM "tfex-cosmos-cassandra-keyspace"."userlogs"')
-        return {'results': 'true'}
-    def eliminarArchivo(self, user):
-        session.execute('SELECT * FROM "tfex-cosmos-cassandra-keyspace"."userlogs"')
-        return {'results': 'true'}
+    def submit(self, user, log):
+        session.execute("INSERT INTO userlogs (user_id, logline) VALUES ('"+user+"', '"+log+"')")
+        return "Submited"
 
-# Agrega la clase de recurso a la API
+    def fileLoaded(self, user):
+        self.submit(user, "File loaded")
+
+    def userLogin(self, user_id):
+        self.submit(user_id, "User logged in")
+
+    def userLogout(self, user_id):
+        self.submit(user_id, "User logged out")
+
+    def enrollCourse(self, user_id):
+        self.submit(user_id, "Course enrolled")
+
+    def availableCourses(self, user_id):
+        self.submit(user_id, "Viewed available courses")
+
+    def cancelEnrollment(self, user_id):
+        self.submit(user_id, "Course enrollment cancelled")
 
 def main():
-    pass
+    c1 = CassandraConnector()
+    
+    c1.fileLoaded("Isaac4918")
 
 if __name__ == '__main__':
     main()
