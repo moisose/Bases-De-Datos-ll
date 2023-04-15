1c4f9587c4a8d6f4cd2a683af76d4a623aa57acd
Moises Solano <90010406+moisose@users.noreply.github.com>
2023-04-14 23:13:34 -0600

    .tf files

---
+++ Proyecto1/infrastructure/main.tf
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b97b083e68cbfac62ef2e29668d6d271c31bb19fab5ae1bb66e39aee547e0c5d
+size 2287
---
+++ Proyecto1/infrastructure/version.tf
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:076bf99d2e52baf52f8e9b23208f94a9338f703f8eada92361812d1837ad0e07
+size 284
