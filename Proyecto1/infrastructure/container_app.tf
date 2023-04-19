ed91266cc077954509ac9d00018a9255f8d12464
Moises Solano <90010406+moisose@users.noreply.github.com>
2023-04-18 22:22:44 -0600

    Removing git lfs files

--- Proyecto1/infrastructure/container_app.tf
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:7a7d141178d9f000ffd4dbdbcae242fddc3ad04f170335ce091563c081977af4
-size 2269
--- Proyecto1/infrastructure/output.tf
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:3879c0188da99a16a24d8b5896b9dfc8dc08911d1514896c43ac4e2b8d2f6e1f
-size 574
