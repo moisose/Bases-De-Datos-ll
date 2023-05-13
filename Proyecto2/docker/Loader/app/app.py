e5916cb124f80b350b8e648389de0d25f18a70f2
MelSaFer <dhnmelfer@estudiantec.cr>
2023-05-12 22:36:07 -0600

    Working on env vars and connection with MongoDB

--- Proyecto2/docker/Loader/app/app.py
+++ Proyecto2/docker/Loader/app/app.py
@@ -18,72 +18,57 @@ Password= os.getenv('PASSWORD')
 DatabaseName= os.getenv('DATABASENAME')
 ArtistsCollection= os.getenv('ARTISTS_COLLECTION')
 LyricsCollection= os.getenv('LYRICS_COLLECTION')
-Artist_File= os.getenv('ARTIST_FILE')
-Lyrics_File= os.getenv('LYRICS_FILE')
 
 uri = "mongodb+srv://" + str(UserName) + ":" + str(Password) + "@mangos.ybmshbl.mongodb.net/" + str(DatabaseName)
-print(uri)
-#uri = "mongodb+srv://MelSaFer:3trZoWOalvOKN7tQ@mangos.ybmshbl.mongodb.net/OpenLyricsSearch"
-client = MongoClient(uri)  #Cliente Mongo
-client = MongoClient(uri, server_api=ServerApi('1'))
-
 connectionString = "DefaultEndpointsProtocol=https;AccountName=filesmanagermangos;AccountKey=KzcNb8ePMdcwCm5gO8/DJc9nY6fngiXFETmDtcdgBfoUPSo+/BowwJxxSdjVx8n0Trh72v9k/yrb+AStjPineQ==;EndpointSuffix=core.windows.net" 
+
+
+
 containerName = "documents"
-pathLyrics = '\\downloadedFiles'
-pathArtists = ""
-fileName = "prueba.txt"
-
-fileDownloaded = False
-
+path_File = '\\downloadedFiles'
+Artist_File="artists-data.csv"
+Lyrics_File="lyrics-data.csv"
+#fileName = "artists-data.csv"
+artistDownloaded = None
+lyricsDownloaded = None
 
 # ===========================================================================
 # Blob Storage
 
 # Downloads a file from the blob storage
-def downloadFiles(filename, filePath):
-    global fileDownloaded
+def downloadFile(filename, filePath):
     try:
+        # Conection with Mongo DB
+        client = MongoClient(uri)  #Mongo Client
+        client = MongoClient(uri, server_api=ServerApi('1'))
+        #Prueba conexión con Mongo DB
+        client.admin.command('ping')
+        print("Pinged your deployment. You successfully connected to MongoDB!") 
+        
+        # Connection with blob storage
         blob_service_client = BlobServiceClient.from_connection_string(connectionString)
         container_client = blob_service_client.get_container_client(containerName)
         blob_client = container_client.get_blob_client(filename)
+        
+        # Opens and reads the archive
         with open(filePath, "wb") as my_blob:
             download_stream = blob_client.download_blob()
             my_blob.write(download_stream.readall())
 
-        fileDownloaded = open(filePath, 'r')
-        return f"File {filename} downloaded to {filePath}"
-    except Exception as e:
-        return {'error': str(e)}
-    
-
-"""
-----------------------------------------------------------------------------------------------
-Function for reading artist.csv
-->Entries:
-->Returns:
-->Restrictions:
-----------------------------------------------------------------------------------------------
-"""
-def readArtist():
-    try:
-        client.admin.command('ping')
-        print("Pinged your deployment. You successfully connected to MongoDB!")
-        
-        
+        cirrentFile = open(filePath, 'r')
         client.close()
+        print(f"File {filename} downloaded to {filePath}")
+        return cirrentFile
     except Exception as e:
         print(e)
-        
+
+
+ 
+ 
     
 def main():
-    print(downloadFiles(fileName, pathLyrics + "\\" + fileName))
-    print(fileDownloaded.read())
-    try:
-        client.admin.command('ping')
-        print("Pinged your deployment. You successfully connected to MongoDB!")
-        client.close()
-    except Exception as e:
-        print(e)
+    artistDownloaded = downloadFile(Artist_File, path_File + "\\" + Artist_File)
+    lyricsDownloaded = downloadFile(Lyrics_File, path_File + "\\" + Lyrics_File)
 
 if __name__ == '__main__': 
     main()
--- Proyecto2/docker/build.sh
+++ Proyecto2/docker/build.sh
@@ -1,8 +1,5 @@
 sudo docker login
 cd Loader
-sudo docker build --build-arg ENV_FILE=envVar.env  -t melanysf/loader-p2 .
-#sudo docker build --env-file envVar.env -t melanysf/loader-p2 .
-#sudo docker build -t melanysf/loader-p2 .
-#sudo docker run --env-file envVar.env melanysf/loader-p2
-#sudo docker images
+sudo docker build --build-arg USERNAME=MelSaFer --build-arg PASSWORD=3trZoWOalvOKN7tQ --build-arg DATABASE=OpenLyricsSearch --build-arg ARTISTS_COLLECTION=artistsCollection --build-arg LYRICS_COLLECTION=lyricsCollection -t melanysf/loader-p2 .
 sudo docker push melanysf/loader-p2
+sudo docker run melanysf/loader-p2
