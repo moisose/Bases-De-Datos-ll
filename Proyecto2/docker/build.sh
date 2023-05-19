46c9f7274ecc122d5e636b1c8b57472b9647350a
MelSaFer <dhnmelfer@estudiantec.cr>
2023-05-19 16:40:12 -0600

    Updates on env variables

--- Proyecto2/docker/build.sh
+++ Proyecto2/docker/build.sh
@@ -2,4 +2,8 @@ sudo docker login
 cd Loader
 sudo docker build --build-arg USERNAME=MelSaFer --build-arg PASSWORD=3trZoWOalvOKN7tQ --build-arg DATABASENAME=OpenLyricsSearch --build-arg ARTISTS_COLLECTION=artistsCollection --build-arg LYRICS_COLLECTION=lyricsCollection -t melanysf/loader-p2 .
 sudo docker push melanysf/loader-p2
-sudo docker run melanysf/loader-p2
+#sudo docker run melanysf/loader-p2
+
+cd ../API
+sudo docker build --build-arg USERNAME=MelSaFer --build-arg PASSWORD=3trZoWOalvOKN7tQ --build-arg DATABASENAME=OpenLyricsSearch --build-arg ARTISTS_COLLECTION=artistsCollection --build-arg LYRICS_COLLECTION=lyricsCollection -t isaac4918/mangos-api .
+sudo docker push isaac4918/mangos-api
