3e38d4c31e6844ac91c26e4aa4225524d8708344
MelSaFer <dhnmelfer@estudiantec.cr>
2023-06-14 14:01:35 -0600

    Updates on backup PostGre

---
+++ Proyecto3/helm/backups/scripts/postGreSQL.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# get the current date and time
+DATE=$(date '+%Y%m%d%H%M')
+# create a directory with the current date
+# the [-p] arg is used bs /pgdump may not exist
+mkdir -p /pgdump/$DATE
+# Update the packages
+apk update
+apk upgrade
+# Install the PostgreSQL client
+apk add postgresql-client
+# Azure CLI command for enable dynamic install without a prompt.
+az config set extension.use_dynamic_install=yes_without_prompt
+# pg_dump configuration for connect to an instance
+#pg_dump --host="$POSTGRES_CONNECTION_STRING" -U $POSTGRES_USERNAME --file=/pgdump/$DATE/database.dump
+az storage blob directory upload --container $CONTAINER -s /pgdump/$DATE -d $BACKUP_PATH --auth-mode key --recursive
+rm -rf /pgdump/$DATE
