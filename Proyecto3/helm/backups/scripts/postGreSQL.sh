b04eacc96cf37d33e8b6262dbddc644520bd8678
Moises Solano <90010406+moisose@users.noreply.github.com>
2023-06-14 16:49:44 -0600

    postgresql upload dump

--- Proyecto3/helm/backups/scripts/postGreSQL.sh
+++ Proyecto3/helm/backups/scripts/postGreSQL.sh
@@ -12,6 +12,6 @@ apk add postgresql-client
 # Azure CLI command for enable dynamic install without a prompt.
 az config set extension.use_dynamic_install=yes_without_prompt
 # pg_dump configuration for connect to an instance
-pg_dump --host="$POSTGRES_CONNECTION_STRING" -U $POSTGRES_USERNAME --file=/pgdump/$DATE/database.dump
+PGPASSWORD="$POSTGRESQL_PASSWORD" pg_dump --host $DB_HOST -U $POSTGRESQL_USERNAME --file=/pgdump/$DATE/database.dump
 az storage blob directory upload --container $CONTAINER -s /pgdump/$DATE -d $BACKUP_PATH --auth-mode key --recursive
 rm -rf /pgdump/$DATE
