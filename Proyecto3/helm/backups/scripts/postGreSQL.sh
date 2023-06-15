ccc35d4677b4fb0170be39fdccdef08a083719d4
MelSaFer <dhnmelfer@estudiantec.cr>
2023-06-15 10:59:02 -0600

    Working on postgresql

--- Proyecto3/helm/backups/scripts/postGreSQL.sh
+++ Proyecto3/helm/backups/scripts/postGreSQL.sh
@@ -17,6 +17,8 @@ PGPASSWORD="$POSTGRESQL_PASSWORD" pg_dumpall --host $DB_HOST -U $POSTGRESQL_USER
 
 # Drop all databases except the default ones
 #PGPASSWORD="$POSTGRESQL_PASSWORD" psql --host $DB_HOST -U $POSTGRESQL_USERNAME -c "SELECT 'DROP DATABASE IF EXISTS ' || datname || ';' FROM pg_database WHERE datistemplate = false AND datname != 'postgres';" | PGPASSWORD="$POSTGRESQL_PASSWORD" psql --host $DB_HOST -U $POSTGRESQL_USERNAME
+# Delete all data from the database
+PGPASSWORD="$POSTGRESQL_PASSWORD" psql --host $DB_HOST -U $POSTGRESQL_USERNAME -d $POSTGRESQL_DBNAME -c "SELECT 'DROP TABLE IF EXISTS ' || tablename || ' CASCADE;' FROM pg_tables WHERE schemaname = 'public';" | PGPASSWORD="$POSTGRESQL_PASSWORD" psql --host $DB_HOST -U $POSTGRESQL_USERNAME -d $POSTGRESQL_DBNAME
 
 az storage blob directory upload --container $CONTAINER -s /pgdump/$DATE -d $BACKUP_PATH --auth-mode key --recursive
 rm -rf /pgdump/$DATE
