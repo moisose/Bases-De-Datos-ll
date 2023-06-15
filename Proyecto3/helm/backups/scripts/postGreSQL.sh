fe12517c84a9ffbf771fb4d85ba069622db60816
MelSaFer <dhnmelfer@estudiantec.cr>
2023-06-15 13:33:27 -0600

    Postgresql backup

--- Proyecto3/helm/backups/scripts/postGreSQL.sh
+++ Proyecto3/helm/backups/scripts/postGreSQL.sh
@@ -11,15 +11,9 @@ apk upgrade
 apk add postgresql-client
 # Azure CLI command for enable dynamic install without a prompt.
 az config set extension.use_dynamic_install=yes_without_prompt
-
 # pg_dump configuration for connect to an instance
 PGPASSWORD="$POSTGRESQL_PASSWORD" pg_dumpall --host $DB_HOST -U $POSTGRESQL_USERNAME --file=/pgdump/$DATE/db_backup.dump
-
-# Drop all databases except the default ones
-#PGPASSWORD="$POSTGRESQL_PASSWORD" psql --host $DB_HOST -U $POSTGRESQL_USERNAME -c "SELECT 'DROP DATABASE IF EXISTS ' || datname || ';' FROM pg_database WHERE datistemplate = false AND datname != 'postgres';" | PGPASSWORD="$POSTGRESQL_PASSWORD" psql --host $DB_HOST -U $POSTGRESQL_USERNAME
-# Delete all data from the database
-PGPASSWORD="$POSTGRESQL_PASSWORD" psql --host $DB_HOST -U $POSTGRESQL_USERNAME -d $POSTGRESQL_DBNAME -c "SELECT 'DROP TABLE IF EXISTS ' || tablename || ' CASCADE;' FROM pg_tables WHERE schemaname = 'public';" | PGPASSWORD="$POSTGRESQL_PASSWORD" psql --host $DB_HOST -U $POSTGRESQL_USERNAME -d $POSTGRESQL_DBNAME
-
 az storage blob directory upload --container $CONTAINER -s /pgdump/$DATE -d $BACKUP_PATH --auth-mode key --recursive
 rm -rf /pgdump/$DATE
+
 
